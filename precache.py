91a0208b12bbe8fb19df91a3bf0ef3f9f4a280ab
carlashley <carl@carlashley.com>
2016-09-26 16:55:55 +1000

    Fixed the small issue of IPSW files not saving to /tmp/precache and did some refactoring on some functions

--- precache.py
+++ precache.py
@@ -46,17 +46,45 @@ class PreCache(object):
             object is initialised.
             Can also override by providing those arguments."""
 
-        self.logger = logging.getLogger('precache')
-        if 'info' in log_level:
-            self.logger.setLevel(logging.INFO)
-        if 'debug' in log_level:
-            self.logger.setLevel(logging.DEBUG)
-        self.fh = logging.FileHandler('/tmp/precache.log')
-        self.formatter = logging.Formatter(
-            '%(asctime)s %(levelname)s - %(message)s'
-        )
-        self.fh.setFormatter(self.formatter)
-        self.logger.addHandler(self.fh)
+        # Logging class
+        class Logger():
+            def __init__(self, log_level='info', log_path='/tmp/precache.log'):
+                self.log_level = log_level
+                self.logger = logging.getLogger('precache')
+
+                # Handle log levels
+                if 'info' in self.log_level:
+                    self.logger.setLevel(logging.INFO)
+
+                if 'debug' in self.log_level:
+                    self.logger.setLevel(logging.DEBUG)
+
+                self.fh = logging.FileHandler(log_path)
+                self.formatter = logging.Formatter(
+                    '%(asctime)s %(levelname)s - %(message)s'
+                )
+
+                self.fh.setFormatter(self.formatter)
+
+                self.logger.addHandler(self.fh)
+
+            # Normal log, and debug log levels
+            def log(self, log_message):
+                self.logger.info(log_message)
+
+            def debug(self, log_message):
+                self.logger.debug(log_message)
+
+            def critical(self, log_message):
+                self.logger.critical(log_message)
+
+        # Configuration of precache
+        l = Logger(log_level='debug', log_path='/tmp/precache.log')
+        self.log = l.log
+        self.debug = l.debug
+        self.critical = l.critical
+
+        # URL Feeds for iOS/tvOS/watchOS
         self.base_feed_url = 'http://mesu.apple.com/assets'
         self.mobile_asset_path = 'com_apple_MobileAsset_SoftwareUpdate'
         self.xml_url = 'com_apple_MobileAsset_SoftwareUpdate.xml'
@@ -72,6 +100,9 @@ class PreCache(object):
                                  self.mobile_asset_path,
                                  self.xml_url),
         }
+
+        # macOS Installer URLs from Mac App Store. These are likely to change
+        # with each macOS release
         self.osx_installer_urls = {
             'MountainLion': {'version': '10.8.5',
                              'url': ['http://osxapps.itunes.apple.com/',
@@ -105,9 +136,10 @@ class PreCache(object):
                                'jze1425880974225146329.pkg']}
         }
 
-        self.user_agent = ('User-Agent', 'PreCacher/1.5')
+        # Caching Server configuration
         self.cache_config_path = '/Library/Server/Caching/Config/Config.plist'
 
+        # Check if the machine is running Caching Server and configure port
         if not cache_server:
             fallback_srv = 'http://localhost:49672'
             if os.path.exists(self.cache_config_path):
@@ -119,74 +151,45 @@ class PreCache(object):
                     self.cache_server = 'http://localhost:%s' % (
                         self.cache_server_port
                     )
-                    self.logger.info(
-                        'Caching server found at: %s' % self.cache_server
-                    )
+                    self.debug('Local machine appears to be a Caching Server')
                 except:
                     self.cache_server = fallback_srv
-                    self.logger.debug(
-                        'Fallback to default cache server: %s' % fallback_srv
-                    )
+                    self.debug('Using fallback caching server %s' %
+                               self.cache_server)
             else:
                 self.cache_server = fallback_srv
-                self.logger.debug(
-                    'Fallback to default cache server %s' % fallback_srv
-                )
+                self.debug('Using fallback caching server %s' %
+                           self.cache_server)
+
         if cache_server:
             self.cache_server = cache_server
 
+        self.log('Using caching server %s' % self.cache_server)
+
         self.exclude_beta = True
+        if self.exclude_beta:
+            self.log('Ignoring beta releases')
+
+        if not self.exclude_beta:
+            self.log('Includng beta releases')
+
+        # Assets master list - where all found items get stored for download
         self.assets_master = []
+
+        # Named tuple for asset creation to drop into self.assets_master
         self.Asset = collections.namedtuple('Asset', ['model',
                                                       'download_url',
                                                       'os_version'])
 
-    def convert_asset_url(self, asset_url):
-        asset_url = urlparse(asset_url)
-        asset_url = '%s%s?source=%s' % (self.cache_server,
-                                        asset_url.path,
-                                        asset_url.netloc)
-        return asset_url
-
-    def cache_ipsw(self, device_model):
-        url = 'http://api.ipsw.me/v2.1/%s/latest/url' % device_model
-        try:
-            opener = urllib2.build_opener()
-            opener.addheaders = [self.user_agent]
-            request = opener.open(url)
-        except urllib2.HTTPError as e:
-            print '%s - Model or IPSW may not exist (%s)' % (e, device_model)
-        else:
-            ipsw_url = urlparse(request.read())
-            os_ver = ipsw_url.path.split('/')[1]
-            ipsw_url = '%s%s?source=%s' % (self.cache_server,
-                                           ipsw_url.path,
-                                           ipsw_url.netloc)
-
-            asset = self.Asset(
-                model=device_model + ' (ipsw)',
-                download_url=ipsw_url,
-                os_version=os_ver
-            )
-
-            return asset
-
-    def download_ipsw(self, device_model):
-        for model in device_model:
-            try:
-                asset = self.cache_ipsw(model)
-                self.download(asset, keep_file=True)
-            except:
-                pass
-
+    # Process the iOS/tvOS/watchOS XML feeds
     def process_update_feed(self, feed_url):
         """ Gets the update feed from Apple.
             If the asset item processed can be cached, it gets added to the
             assets_master list that is empty when initialised.
         """
         try:
+            self.debug('Started processing update feed %s' % feed_url)
             response = urllib2.urlopen(feed_url)
-            self.logger.debug('Opening URL %s' % feed_url)
             feed_data = plistlib.readPlistFromString(response.read())
 
             for item in feed_data['Assets']:
@@ -218,43 +221,31 @@ class PreCache(object):
                         if asset not in self.assets_master:
                             if not item.get('ReleaseType') == 'Beta':
                                 self.assets_master.append(asset)
-                                self.logger.debug(
-                                    'Added %s to master assets' % url
+                                self.debug(
+                                    'Added asset URL %s' % url
                                 )
 
                 if 'Watch' in asset.model:
                     if asset not in self.assets_master:
                         if not item.get('ReleaseType') == 'Beta':
                             self.assets_master.append(asset)
-                            self.logger.debug(
-                                'Added %s to master assets' % url
+                            self.debug(
+                                'Added asset %s URL %s' % (asset.model, url)
                             )
+                self.debug('Completed processing update feed %s' % feed_url)
 
         except (urllib2.URLError, urllib2.HTTPError) as e:
-            self.logger.debug('Error processing assets: %s' % e)
+            self.debug('Exception (%s) processing feed %s' % (e, feed_url))
             print '%s' % e
             exit(1)
 
     def build_asset_master_list(self):
+        # iOS/tvOS/watchOS
         for item in self.update_feeds:
+            self.debug('Processing item %s' % item)
             self.process_update_feed(self.update_feeds[item])
 
-    def list_devices_in_feed(self):
-        assets_list = []
-
-        self.build_asset_master_list()
-        self.build_osx_assets()
-
-        for item in self.assets_master:
-            if item.model not in assets_list:
-                assets_list.append(item.model)
-
-        assets_list.sort()
-        print 'iOS and tvOS models:'
-        for i in assets_list:
-            print i
-
-    def build_osx_assets(self):
+        # macOS Installers
         for installer in self.osx_installer_urls:
             os_ver = self.osx_installer_urls[installer]['version']
             url = ''.join(self.osx_installer_urls[installer]['url'])
@@ -266,16 +257,31 @@ class PreCache(object):
             )
             if asset not in self.assets_master:
                 self.assets_master.append(asset)
+                self.debug('Added asset %s URL %s' % (asset.model,
+                                                      asset.download_url))
 
-    def cache_osx(self, osx_installer):
-        self.build_osx_assets()
-        for m in osx_installer:
-            for item in self.assets_master:
-                if m in item.model:
-                    self.logger.info(
-                        'Caching %s %s' % (item.model, item.os_version)
-                    )
-                    self.download(item)
+    def convert_asset_url(self, asset_url):
+        asset_url = urlparse(asset_url)
+        asset_url = '%s%s?source=%s' % (self.cache_server,
+                                        asset_url.path,
+                                        asset_url.netloc)
+        self.debug('Converted asset url to %s' % asset_url)
+        return asset_url
+
+    # Function for listing assets available to be cached
+    def list_devices_in_feed(self):
+        assets_list = []
+
+        self.build_asset_master_list()
+
+        for item in self.assets_master:
+            if item.model not in assets_list:
+                assets_list.append(item.model)
+
+        assets_list.sort()
+        print 'iOS and tvOS models:'
+        for i in assets_list:
+            print i
 
     def convert_size(self, file_size, precision=2):
         """ Converts the size of remote object to human readable format"""
@@ -293,31 +299,24 @@ class PreCache(object):
             if not os.path.isdir(download_dir):
                 os.mkdir(download_dir)
                 local_file = os.path.join(download_dir, local_file)
+                f = open(local_file, 'wb')
+                self.log('Saving IPSW %s to %s' % (remote_file, local_file))
                 print 'IPSW saving to %s' % local_file
         if not keep_file:
             local_file = os.path.join('/dev/null', local_file)
+
         try:
             if ('.zip' or '.ipsw' or '.xip' in remote_file):
-                opener = urllib2.build_opener()
-                opener.addheaders = [self.user_agent]
-                req = opener.open(remote_file)
-                self.logger.debug("Looking for Content-Type header")
+                req = urllib2.urlopen(remote_file)
                 if req.info().getheader('Content-Type') is not None:
-                    self.logger.debug(
-                        'No Content-Type header found... caching'
-                    )
                     try:
-                        self.logger.debug(
-                            'Trying to get remote file size attempt 1'
-                        )
+                        self.debug('Attempting to fetch %s' % remote_file)
                         ts = req.info().getheader('Content-Length').strip()
                         human_fs = self.convert_size(float(ts))
                         header = True
                     except AttributeError:
                         try:
-                            self.logger.debug(
-                                'Trying to get remote file size attempt 2'
-                            )
+                            self.debug('Attempting to fetch %s' % remote_file)
                             ts = req.info().getheader('Content-Length').strip()
                             human_fs = self.convert_size(float(ts))
                             header = True
@@ -327,8 +326,9 @@ class PreCache(object):
                     if header:
                         ts = int(ts)
                     bytes_so_far = 0
-                    self.logger.debug(
-                        'Beggining download of %s' % remote_file
+                    self.log('Starting download of %s to %s' % (
+                            remote_file, local_file
+                        )
                     )
                     while True:
                         buffer = req.read(8192)
@@ -337,6 +337,9 @@ class PreCache(object):
                             break
 
                         bytes_so_far += len(buffer)
+                        if keep_file:
+                            f.write(buffer)
+
                         if not header:
                             ts = bytes_so_far
 
@@ -352,21 +355,17 @@ class PreCache(object):
                         )
                         stdout.flush()
 
-                    self.logger.info(
-                        'Cached %s %s - %s' % (asset.model,
-                                               asset.os_version,
-                                               remote_file)
+                    self.log(
+                        'Cached %s %s from %s' % (asset.model,
+                                                  asset.os_version,
+                                                  remote_file)
                     )
                 else:
                     print 'Skipping %s - already in cache' % asset.model
-                    self.logger.info(
-                        'Skipping %s %s - already in cache' % (asset.model,
-                                                               remote_file)
+                    self.log(
+                        'Already in cache %s %s' % (asset.model, remote_file)
                     )
             else:
-                self.logger.debug(
-                    'Falling back to no progress download method'
-                )
                 req = urllib2.urlopen(remote_file)
                 print 'Caching %s (%s)' % (asset.model[0],
                                            asset.os_version)
@@ -380,13 +379,17 @@ class PreCache(object):
                     """You may need to specify the cache server """
                     """with the -cs or --caching-server flag. """
                 )
+                self.log(
+                    'Connection refused. Check Caching Server URL is correct'
+                )
             elif errno.ETEIMDOUT:
                 print (
                     """Error: Connection timed out. Try again later."""
                 )
+                self.log('Connection timed out. Try again later.')
             else:
                 print '%s' % e
-            self.logger.debug('Error downloading file - %s' % e)
+            self.debug('Exception (%s) downloading %s' % (e, remote_file))
             exit(1)
         sleep(0.05)
 
@@ -394,16 +397,53 @@ class PreCache(object):
         self.build_asset_master_list()
 
         if model:
-            self.logger.info('Caching models %s' % model)
+            self.log('Caching model %s' % model)
             for m in model:
                 for item in self.assets_master:
                     if m in item.model:
                         self.download(item)
         else:
             print 'Caching all models'
-            self.logger.info('Caching all models')
+            self.log('Caching all models')
             for item in self.assets_master:
                 self.download(item)
+
+    # Functions for downloading IPSW's
+    def cache_ipsw(self, device_model):
+        url = 'http://api.ipsw.me/v2.1/%s/latest/url' % device_model
+        req = urllib2.urlopen(url)
+        ipsw_url = urlparse(req.read())
+        os_ver = ipsw_url.path.split('/')[1]
+        ipsw_url = '%s%s?source=%s' % (self.cache_server,
+                                       ipsw_url.path,
+                                       ipsw_url.netloc)
+
+        asset = self.Asset(
+            model=device_model + ' (ipsw)',
+            download_url=ipsw_url,
+            os_version=os_ver
+        )
+
+        return asset
+
+    def download_ipsw(self, device_model):
+        for model in device_model:
+            try:
+                asset = self.cache_ipsw(model)
+                self.download(asset, keep_file=True)
+            except Exception as e:
+                self.debug('Not sure what error comes up here, so bam: %s' % e)
+                pass
+
+    # Function for downloading macOS installer from Mac App Store
+    def cache_osx(self, osx_installer):
+        for m in osx_installer:
+            for item in self.assets_master:
+                if m in item.model:
+                    self.log(
+                        'Caching %s %s' % (item.model, item.os_version)
+                    )
+                    self.download(item)
 
 
 def main():
@@ -485,18 +525,22 @@ def main():
 
     if len(argv) > 1:
         try:
-            srv = args.cache_server[0]
-            pop = PreCache(cache_server=srv)
-        except:
-            pop = PreCache()
-        if args.list_models:
-            pop.list_devices_in_feed()
-        if args.model:
-            pop.cache_asset(args.model)
-        if args.ipsw:
-            pop.download_ipsw(args.ipsw)
-        if args.os:
-            pop.cache_osx(args.os)
+            try:
+                srv = args.cache_server[0]
+                pop = PreCache(cache_server=srv)
+            except:
+                pop = PreCache()
+            if args.list_models:
+                pop.list_devices_in_feed()
+            if args.model:
+                pop.cache_asset(args.model)
+            if args.ipsw:
+                pop.download_ipsw(args.ipsw)
+            if args.os:
+                pop.cache_osx(args.os)
+        except KeyboardInterrupt:
+            print ''
+            exit(1)
 
     else:
         parser.print_usage()
